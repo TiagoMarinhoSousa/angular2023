9c30bd805a6f872e6c28d6a0e55888b93c07c583
TiagoMarinhoSousa <tiagomarinho9101@gmail.com>
2023-03-29 16:59:40 -0300

    Formularios reativos

--- 01_CRUD/memoteca/src/app/app.module.ts
+++ 01_CRUD/memoteca/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
@@ -28,7 +28,8 @@ import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pens
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
